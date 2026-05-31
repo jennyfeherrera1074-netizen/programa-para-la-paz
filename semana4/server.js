8cdf9b61f0c7c3979ac612f1ca9a47b7c7ba0605
jennyfeherrera1074-netizen <jennyfeherrera1074@gmail.com>
2026-05-31 14:17:31 +0000

    actividad semana4

---
+++ semana4/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente');
+  });
+
+  app.listen(3000, () => {
+    console.log('Servidor ejecutandose en puerto 3000');
+    });
+
+    
+         
+         
+         
+     
